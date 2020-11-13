8c72ed68085f74c654af241bcb98f9ca846227b1
Mario <mario.tota.mt@gmail.com>
2020-11-13 16:20:09 +0100

    commit

--- script.js
+++ script.js
@@ -18,29 +18,37 @@ var costoBigliettoanziani = costoBiglietto - ((costoBiglietto * 40) / 100);
 costoBigliettoanziani = costoBigliettoanziani.toFixed(2);
 console.log("costo per gli anziani ", costoBigliettoanziani)
 
-var minorenne = document.getElementById("minorenne");
-var maggiorenne = document.getElementById("maggiorenne");
-var over65 = document.getElementById("over65");
+// var minorenne = document.getElementById("minorenne");
+// var maggiorenne = document.getElementById("maggiorenne");
+// var over65 = document.getElementById("over65");
+
+
+var fasceArray = ["minorenne", "maggiorenne", "over65"];
 
 var element = document.getElementById("genera");
 element.addEventListener('click',
 
   function() {
-
     document.getElementById('spazionomepasseggero').innerHTML=nomeCognome;
 
-    if (fasciaEta = minorenne) {
+    if (fasciaEta == fasceArray[0]) {
       document.getElementById('spazioofferta').innerHTML="Offerta minorenni";
       document.getElementById('spaziocostobiglietto').innerHTML="&euro; " + costoBigliettoMinor;
-    }
-    else if(fasciaEta = over65) {
-      document.getElementById('spazioofferta').innerHTML="Offerta over65";
-      document.getElementById('spaziocostobiglietto').innerHTML="&euro; " + costoBigliettoanziani;
-    }
-    else {
-      document.getElementById('spaziocostobiglietto').innerHTML="&euro; " + costoBiglietto;
-      document.getElementById('spazioofferta').innerHTML="Biglietto ordinario";
 
+    if (fasciaEta == fasceArray[1]) {
+      document.getElementById('spazioofferta').innerHTML="biglietto ordinario";
+      document.getElementById('spaziocostobiglietto').innerHTML="&euro; " + costoBiglietto;
+    }
+
+    // if(fasciaEta = over65) {
+    //   document.getElementById('spazioofferta').innerHTML="Offerta over65";
+    //   document.getElementById('spaziocostobiglietto').innerHTML="&euro; " + costoBigliettoanziani;
+    // }
+    //
+    // if(fasciaEta = maggiorenne) {
+    //   document.getElementById('spaziocostobiglietto').innerHTML="&euro; " + costoBiglietto;
+    //   document.getElementById('spazioofferta').innerHTML="Biglietto ordinario";
+    // }
     }
   }
 );
