98178569d2037308e1a10a1d50f99c97cebf7048
Mario <mario.tota.mt@gmail.com>
2020-11-14 13:45:13 +0100

    commit

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 var fasceArray = ["minorenne", "maggiorenne", "over65"];
 
-var element = document.getElementById("genera");
-element.addEventListener('click',
+var conseguenzeClickGenera = document.getElementById("genera");
+conseguenzeClickGenera.addEventListener('click',
 
   function() {
     var fasciaEta = document.getElementById("eta").value;
     var nomeCognome = document.getElementById("nome").value;
     var kmDaPercorrere = document.getElementById("km").value;
+
     var costoBiglietto = kmDaPercorrere * 0.21;
     var costoBigliettoMinor = costoBiglietto - ((costoBiglietto * 20) / 100);
     costoBigliettoMinor = costoBigliettoMinor.toFixed(2);
     var costoBigliettoAnziani = costoBiglietto - ((costoBiglietto * 40) / 100);
     costoBigliettoAnziani = costoBigliettoAnziani.toFixed(2);
+
+    var numeroCarrozza = Math.floor(Math.random() * 10);
+    document.getElementById("carrozza").innerHTML = numeroCarrozza;
+    var codiceTreno = Math.floor(Math.random() * 10000) + 90000;
+    document.getElementById("codicetreno").innerHTML = codiceTreno;
 
     document.getElementById('spazionomepasseggero').innerHTML=nomeCognome;
 
@@ -30,15 +36,13 @@ element.addEventListener('click',
       document.getElementById('spaziocostobiglietto').innerHTML="&euro; " + costoBigliettoAnziani;
     }
 
-    var numeroCarrozza = Math.floor(Math.random() * 10);
-    document.getElementById("carrozza").innerHTML = numeroCarrozza;
-    var codiceTreno = Math.floor(Math.random() * 10000) + 90000;
-    document.getElementById("codicetreno").innerHTML = codiceTreno;
+    var impostaVisibilita = document.getElementById("biglietto");
+    impostaVisibilita.className = "visible";
   }
 );
 
-var elementB = document.getElementById("annulla");
-elementB.addEventListener('click',
+var conseguenzeClickAnnulla = document.getElementById("annulla");
+conseguenzeClickAnnulla.addEventListener('click',
 
   function() {
     var nomeCognome = document.getElementById("nome").value;
@@ -53,5 +57,8 @@ elementB.addEventListener('click',
 
     document.getElementById("carrozza").innerHTML = "";
     document.getElementById("codicetreno").innerHTML = "";
+
+    var impostaInvisibilita = document.getElementById("biglietto");
+    impostaInvisibilita.className = "hidden";
   }
 );
