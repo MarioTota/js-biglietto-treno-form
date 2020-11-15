107720854497e2b61bd800dc7609dfa715acfe11
Mario <mario.tota.mt@gmail.com>
2020-11-15 15:14:18 +0100

    commit

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ var impostaVisibilita = document.getElementById("biglietto");
 var impostaInvisibilita = document.getElementById("biglietto");
 impostaInvisibilita.className = "hidden";
 document.getElementById("eta").value = "";
+
 var conseguenzeClickGenera = document.getElementById("genera");
 conseguenzeClickGenera.addEventListener('click',
 
   function() {
-    var fasciaEta = document.getElementById("eta").value;
     var nomeCognome = document.getElementById("nome").value;
     var kmDaPercorrere = document.getElementById("km").value;
+    var fasciaEta = document.getElementById("eta").value;
 
     var costoBiglietto = kmDaPercorrere * 0.21;
     var costoBigliettoMinor = costoBiglietto - ((costoBiglietto * 20) / 100);
@@ -17,34 +18,51 @@ conseguenzeClickGenera.addEventListener('click',
     var costoBigliettoAnziani = costoBiglietto - ((costoBiglietto * 40) / 100);
     costoBigliettoAnziani = costoBigliettoAnziani.toFixed(2);
 
+    // stampa i numeri random
     var numeroCarrozza = Math.floor(Math.random() * 10);
     document.getElementById("carrozza").innerHTML = numeroCarrozza;
     var codiceTreno = Math.floor(Math.random() * 10000) + 90000;
     document.getElementById("codicetreno").innerHTML = codiceTreno;
+    // /stampa i numeri random
 
+    // stampa il nome e il cognome
     document.getElementById('spazionomepasseggero').innerHTML=nomeCognome;
+    // /stampa il nome e il cognome
 
-    if (nomeCognome == "") {
-      window.alert("Devi inserire Nome e Cognome");
-      impostaInvisibilita.className = "hidden";
-    } else {
-      impostaVisibilita.className = "visible";
-    }
-
+    // calcola il prezzo del biglietto in base all'eà
     if (fasciaEta == fasceArray[0]) {
       document.getElementById('spazioofferta').innerHTML="Sconto Minorenni";
       document.getElementById('spaziocostobiglietto').innerHTML="&euro; " + costoBigliettoMinor;
     }
-
     if (fasciaEta == fasceArray[1]) {
       document.getElementById('spazioofferta').innerHTML="Biglietto Ordinario";
       document.getElementById('spaziocostobiglietto').innerHTML="&euro; " + costoBiglietto;
     }
-
     if (fasciaEta == fasceArray[2]) {
       document.getElementById('spazioofferta').innerHTML="Sconto Over-65";
       document.getElementById('spaziocostobiglietto').innerHTML="&euro; " + costoBigliettoAnziani;
     }
+    // /calcola il prezzo del biglietto in base all'eà
+
+    if (nomeCognome == "" || kmDaPercorrere == "" || fasciaEta == "" ) {
+      window.alert("Devi compilare tutti i campi");
+      impostaInvisibilita.className = "hidden";
+      } else {
+        impostaVisibilita.className = "visible";
+        }
+
+    // if (!isNaN(nomeCognome)) {
+    //   window.alert("Nome Cognome: Puoi inserire solo lettere");
+    //   impostaInvisibilita.className = "hidden";
+    //   } else {
+    //     impostaVisibilita.className = "visible";
+    //   }
+    // if (isNaN(kmDaPercorrere)) {
+    //   window.alert("Km da percorrere: Puoi inserire solo numeri");
+    //   impostaInvisibilita.className = "hidden";
+    //   } else {
+    //     impostaVisibilita.className = "visible";
+    //   }
 
   }
 );
